f4523b430993bcc943d940b04d4e19b1f70260bc
Timsel1 <timmeijvogel2003@gmail.com>
2026-03-19 13:04:40 +0100

    Remove all lit files
    starting over in react

--- src/pages/flappy-burd-leaderboard.ts
+++
@@ -1,9 +0,0 @@
-import { LitElement, html } from "lit";
-import { customElement } from "lit/decorators.js";
-
-@customElement("leaderboard-page")
-export class Leaderboard extends LitElement {
-  render() {
-    return html` <p>Leaderboard</p> `;
-  }
-}
--- src/pages/flappy-burd.ts
+++
@@ -1,9 +0,0 @@
-import { LitElement, html } from "lit";
-import { customElement } from "lit/decorators.js";
-
-@customElement("flappy-burd-page")
-export class FlappyBurd extends LitElement {
-  render() {
-    return html` <p>Flappy Burd</p> `;
-  }
-}
