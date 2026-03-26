6d919b38d8508a2f06bb26a66fbdcde0652607f1
Timsel1 <timmeijvogel2003@gmail.com>
2026-03-26 18:01:11 +0100

    Update Flappy Burd html to be fullscreen

--- src/pages/FlappyBurd.tsx
+++ src/pages/FlappyBurd.tsx
@@ -3,11 +3,12 @@ import styles from "./FlappyBurd.module.css";
 export default function FlappyBird() {
   return (
     <div className={styles.frame}>
+      <p>This game is meant to be played as a discord activity</p>
       <iframe
         src="/Unity/index.html"
         style={{
-          width: "100%",
-          height: "100%",
+          width: "960",
+          height: "600",
           border: "none",
         }}
         allow="fullscreen; autoplay"
